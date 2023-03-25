d059638a15a527719c2612a6ac1927284e4bd0df
Tyler Lin <tylerhlin@gmail.com>
2023-03-24 23:07:10 -0400

    Update daily trivia

--- writer.js
+++ writer.js
@@ -21,8 +21,7 @@ const dateString = new Intl.DateTimeFormat(undefined, {
 const data = fs.readFileSync('README-template.md', 'utf8')
 
 const newData = data.replace('_Question Here_', 
-`## :mortar_board: Daily Trivia!
-*Updated ${dateString} at 12:00 AM*
+`*Updated ${dateString} at 12:00 AM*
 ### ${question.question}
 <details>
 <summary>
@@ -51,22 +50,7 @@ ${question.D}
 </summary>
 
 ${meta.D}
-</details>
-
-## :bar_chart: My Stats
-
-![tytot's Github Stats](https://github-readme-stats.vercel.app/api?username=tytot&hide=stars&hide_title=true&show_icons=true&count_private=true&title_color=e31837&icon_color=e31837&text_color=c4ced4&bg_color=002b5c)
-
-## :gem: My Favorite Repos
-
-[![Attendance for Google Meet by tytot](https://github-readme-stats.vercel.app/api/pin/?username=tytot&repo=attendance-for-google-meet&title_color=ffb81c&text_color=ffffff&icon_color=ffb81c&bg_color=6f263d)](https://github.com/tytot/attendance-for-google-meet)
-[![tytot.github.io by tytot](https://github-readme-stats.vercel.app/api/pin/?username=tytot&repo=tytot.github.io&title_color=ff3c00&text_color=ffffff&icon_color=ff3c00&bg_color=311d00)](https://github.com/tytot/tytot.github.io)
-[![Minecraft Proximity Discord Chat by tytot](https://github-readme-stats.vercel.app/api/pin/?username=tytot&repo=minecraft-proximity-discord-chat&title_color=7289da&text_color=ffffff&icon_color=7289da&bg_color=2c2f33)](https://github.com/tytot/minecraft-proximity-discord-chat)
-[![Medical Record Blockchain by tytot](https://github-readme-stats.vercel.app/api/pin/?username=tytot&repo=medical-record-blockchain&title_color=96bb7c&text_color=ffffff&icon_color=96bb7c&bg_color=184d47)](https://github.com/tytot/medical-record-blockchain)
-
-## :scroll: My Most Used Languages
-
-![tytot's Most Used Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=tytot&hide_title=true&layout=compact&text_color=ffffff&bg_color=241773)`)
+</details>`)
 
 fs.writeFile('README.md', newData, (err) => {
     if (err) {
